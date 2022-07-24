6c546a307dee8c14c05ad69e348a200abc12d0c1
Yongsoo Cho <97702731+Yongsoo-Cho@users.noreply.github.com>
2022-07-23 21:28:24 -0600

    Update question347.java

--- Leetcode Folder/question347.java
+++ Leetcode Folder/question347.java
@@ -2,13 +2,19 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int num: nums){ map.put(num, map.getOrDefault(num, 0)+1); }
-
-        PriorityQueue<Integer> heap = new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
+        
+        PriorityQueue<Map.Entry<Integer,Integer>> heap = new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
         heap.addAll(map.entrySet());
-
-        ArrayList<Integer> res = new ArrayList<>();
-        for(int i=0; i<k; i++){ res.add(heap.poll().getKey()); }
-
+        
+        ArrayList<Integer> resList = new ArrayList<>();
+        for(int i=0; i<k; i++){ resList.add(heap.poll().getKey()); }
+        
+        int[] res = new int[resList.size()];
+        
+        for(int i=0; i<res.length; i++){
+            res[i] = resList.get(i);
+        }
+        
         return res;
     }
 }
